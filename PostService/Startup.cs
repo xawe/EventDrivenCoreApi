98541d3b22ebe03bc57e526f931d2360d9cfe1c2
Xawe <dreamxawe@hotmail.com>
2021-06-03 17:38:39 -0300

    implementando post service

--- PostService/Startup.cs
+++ PostService/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PostService
 {
@@ -27,6 +22,10 @@ namespace PostService
         {
 
             services.AddControllers();
+
+            var sqlConnectionString = Configuration.GetConnectionString("PostgreSqlConnectionString");
+            services.AddDbContext<PostService.Data.PostServiceContext>(options => options.UseNpgsql(sqlConnectionString));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PostService", Version = "v1" });
