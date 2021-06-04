ec8f7638b12b0e46c07d4c2d52f7ca911749d5a9
Xawe <dreamxawe@hotmail.com>
2021-06-04 18:33:06 -0300

    Refactor de postController

---
+++ PostService/Data/PostData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostService.Data
+{
+    public class PostData : IPostData
+    {
+        private readonly PostServiceContext _context;
+
+        public PostData(PostServiceContext context)
+        {
+            _context = context;
+        }
+        public async Task<Post> AddPost(Post post)
+        {
+            post.PostId = 1;
+            if (_context.Post.Any())
+            {
+                post.PostId = _context.Post.Max(x => x.PostId) + 1;
+            }
+
+            _context.Post.Add(post);
+            await _context.SaveChangesAsync();
+            return post;            
+        }
+
+        public async Task<List<Post>> GetAllPosts()
+        {
+            return await _context.Post.Include(x => x.User).ToListAsync();           
+        }
+    }
+}
